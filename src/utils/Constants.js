13611cf1ee2f400ee0c150c88c2e3d08998444b4
blackboy <1169794338@qq.com>
2019-05-03 10:18:54 +0800

    配置的抽取 登陆功能的实现

---
+++ src/utils/Constants.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Icon } from 'antd';
+import { formatMessage } from 'umi-plugin-react/locale';
+
+const Constants = {
+  baseUrl: 'http://localhost:9991/api',
+  siteName: '网站名称',
+  description: 'Ant Design 是西湖区最具影响力的 Web 设计规范',
+  copyright: ' 2019 蚂蚁金服体验技术部出品',
+  loginPage: {
+    links: [
+      {
+        key: 'help',
+        title: formatMessage({ id: 'layout.user.link.help' }),
+        href: '',
+      },
+      {
+        key: 'privacy',
+        title: formatMessage({ id: 'layout.user.link.privacy' }),
+        href: '',
+      },
+      {
+        key: 'terms',
+        title: formatMessage({ id: 'layout.user.link.terms' }),
+        href: '',
+      },
+    ],
+  },
+  footer: {
+    links: [
+      {
+        key: 'Pro 首页',
+        title: 'Pro 首页1234',
+        href: 'https://pro.ant.design',
+        blankTarget: true,
+      },
+      {
+        key: 'github',
+        title: <Icon type="github" />,
+        href: 'https://github.com/ant-design/ant-design-pro',
+        blankTarget: true,
+      },
+      {
+        key: 'Ant Design',
+        title: 'Ant Design',
+        href: 'https://ant.design',
+        blankTarget: true,
+      },
+    ],
+  },
+};
+
+export default Constants;
