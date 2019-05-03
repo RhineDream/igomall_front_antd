c816f986addb04fffc5217f847cc71ed3bfc1679
blackboy <1169794338@qq.com>
2019-05-03 10:17:28 +0800

    配置的抽取 登陆功能的实现

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -9,7 +9,7 @@ module.exports = {
   menu: {
     disableLocal: false,
   },
-  title: 'Ant Design Pro',
+  title: '这里是页面标题',
   pwa: false,
   // Your custom iconfont Symbol script Url
   // eg：//at.alicdn.com/t/font_1039637_btcrd5co4w.js
