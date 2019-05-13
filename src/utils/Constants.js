8273a1c1d9d4a7c12346fcee62398e4963f09aae
1169794338@qq.com <Liziyi521521>
2019-05-13 14:31:29 +0800

    调整权限控制

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -3,7 +3,7 @@ import { Icon } from 'antd';
 import { formatMessage } from 'umi-plugin-react/locale';
 
 const Constants = {
-  baseUrl: 'http://localhost:9991/api',
+  baseUrl: 'http://localhost:8080/api',
   siteName: '网站名称',
   description: 'Ant Design 是西湖区最具影响力的 Web 设计规范',
   copyright: ' 2019 蚂蚁金服体验技术部出品',
